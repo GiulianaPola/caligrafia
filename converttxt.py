eb52516c7b10eab5f98ad1d488ea387d39d90154
Giuliana Pola <giulianalopespola@gmail.com>
2022-04-13 22:09:49 -0300

    AAAAAAAAA

--- converttxt.py
+++ converttxt.py
@@ -1,4 +1,4 @@
-txt="Bioquímica 2 - Atividade 3\nGiuliana Lopes Pola 11779802\nDesenhos nas últimas páginas!"
+txt="1. Variações no número de moléculas enzimáticas ou mudanças na atividade catalítica de cada molécula enzimática presente atualmente podem afetar o fluxo através de um processo catalisado por enzimas. Mudanças na concentração local de uma pequena molécula: um substrato da rota na qual essa reação é um passo, um produto do caminho, ou um metabólito ou cofator crítico que reflete o estado metabólico da célula, muitas vezes geram mudanças alostéricas muito rápidas na atividade enzimática. A regulação alostérica é mediada por segundos mensageiros produzidos intracelularmente em resposta a sinais extracelulares (1), em uma escala de tempo um pouco mais lenta ditada pelo ritmo do processo de transdução do sinal. Sinais hormonais ou neurais, assim como fatores de crescimento ou citocinas, são exemplos de sinais extracelulares. As taxas relativas de síntese e decomposição de uma enzima específica determinam a quantidade de moléculas dessa enzima em uma célula. A ativação de um fator de transcrição (2) em resposta a um sinal externo pode alterar a taxa de síntese. Os fatores de transcrição são proteínas nucleares que, quando ativadas, ligam determinado DNA ao promotor de um gene e ativam ou reprimem a transcrição desse gene, resultando no aumento ou redução da síntese da proteína codificada. A ativação de um fator de transcrição pode ocorrer como consequência de sua ligação a um ligante particular ou como resultado de sua fosforilação ou desfosforilação. A estabilidade dos RNAs mensageiros (3), ou sua resistência à destruição por ribonucleases celulares, varia, e a quantidade de mRNA em uma célula é em função de sua taxa de produção e de decomposição. A taxa na qual os ribossomos (4) transcrevem um mRNA em uma proteína é igualmente controlada, e é influenciada por uma série de variáveis. As taxas de decomposição da proteína (5) variam de acordo com a proteína e as circunstâncias na célula. A ligação covalente da ubiquitina a certas proteínas marca a sua decomposição em proteasomas. O sequestro da enzima e de seu substrato em compartimentos separados (6) é outra forma de alterar a atividade efetiva da enzima. Certas enzimas e sistemas enzimáticos são segregados dentro das células por compartimentos ligados por membranas, e o transporte do substrato através dessas membranas intracelulares pode ser o fator limitante na função enzimática. A concentração de seu substrato afeta todas as enzimas (7). Quando a concentração do substrato é menor que o Km, a atividade diminui e a taxa de reação se torna linearmente dependente da concentração do substrato. Um efetor alostérico (8) pode ou aumentar ou reduzir a atividade enzimática. A cinética hiperbólica é frequentemente convertida em cinética sigmóide por efetores alostéricos, e vice versa. Uma pequena alteração na concentração do substrato ou do efetor alostérico pode ter uma influência substancial na taxa de reação na seção mais íngreme da curva sigmóide. Em segundos ou minutos após um sinal regulatório, geralmente um sinal extracelular, ocorrem mudanças covalentes de enzimas ou outras proteínas (9). A fosforilação e a desfosforilação são de longe as mudanças mais prevalentes. A célula deve ser capaz de devolver a enzima alterada ao seu estado de atividade anterior para que a modificação covalente seja benéfica na regulação. Por fim, a conexão e a dissociação de outra proteína reguladora regula várias enzimas (10)."
 import packages
 nfile=0
 
@@ -7,7 +7,7 @@ try:
   import re
   import os
   from unidecode import unidecode
-  from PIL import Image#Open the text file which you have to convert into handwriting
+  from PIL import Image,ImageEnhance#Open the text file which you have to convert into handwriting
   import cv2
   import numpy as np
 except:
@@ -61,7 +61,7 @@ def sylsplit(word):
     #print(word)
     vogals='aãáàâeéèêiíìîoóòõôuAÁÀÃÂEÉÈÊIÍÌÎOÓÒÕÔU'
     syllables=[]
-    if len(word)<3 or word==word.upper():
+    if len(word)<=3 or word==word.upper():
         syllables.append(word)
     else:
         j=0
@@ -109,7 +109,7 @@ sizehyp=sizeletters["{}.png".format(str(ord('-')))]
 sheet_width=BG.width
 sheet_height=BG.height
 gap, ht = 0, 0
-end=False
+endword=False
 last=''
 for word in txt.split(' '):
     if word=='\n':
@@ -125,10 +125,6 @@ for word in txt.split(' '):
         else:
             gap,ht=0,ht+147
         last="\n"
-    elif len(word)==1:
-        writeletter(word,False,gap,ht)
-        last=word
-        gap+=sizeletters['{}.png'.format(str(ord(word)))]-5
     elif '-' in word:
         ws=[]
         words=word.split('-')
@@ -143,10 +139,10 @@ for word in txt.split(' '):
     if not syllables==[]:
         for syl in syllables:
             upperl=False
-            if syl==syl.upper() and len(syl)>1:
+            if len(syl)>1 and not syl[1:]==syl[1:].lower():
                 upperl=True
             #print(syl, last)
-            end=False
+            endword=False
             sizesyl=[]
             for letter in syl:
                 try:
@@ -162,8 +158,8 @@ for word in txt.split(' '):
             else:
                 newgap=gap+sum(sizesyl)+sizehyp
             if sheet_width < newgap:
-                #print(True)
-                if end==False and (last.isalpha() or last.isnumeric()):
+                endline=True
+                if endword==False and (last.isalpha() or last.isnumeric()):
                     try:
                         cases = Image.open("newfont/{}.png".format(str(ord('-'))))
                     except:
@@ -174,7 +170,7 @@ for word in txt.split(' '):
                         BG.paste(cases, (gap, ht),cases)
                         last='hypen'
                         gap+=sizeletters['{}.png'.format(str(ord('-')))]-5
-                if ht+160>=sheet_height:
+                if ht+152>=sheet_height:
                     if nfile==0:
                         BG.save("{}.png".format(slugify(' '.join(txt.split(' ')[0:6]))))
                     else:
@@ -198,12 +194,10 @@ for word in txt.split(' '):
                     last=letter
                     gap+=sizeletters['{}.png'.format(str(ord(letter)))]-5
     syllables=[]
-    end=True
-    if not word=="\n":
-        writeletter(' ',False,gap,ht)
-        last=' '
-        gap+=sizeletters['{}.png'.format(str(ord(' ')))]-5
-BG.show()
+    endword=True
+    writeletter(' ',False,gap+5,ht)
+    last=' '
+    gap+=sizeletters['{}.png'.format(str(ord(' ')))]-5
 if nfile==0:
     BG.save("{}.png".format(slugify(' '.join(txt.split(' ')[0:6]))))
 else:
@@ -214,3 +208,8 @@ my_dir = os.getcwd()
 for fname in os.listdir(my_dir):
     if fname.startswith(slugify(' '.join(txt.split(' ')[0:6]))):
         removebg.remove(os.path.join(my_dir, fname),False)
+        img = Image.open(fname)
+        converter = ImageEnhance.Color(img)
+        img2 = converter.enhance(0.5)
+        #img2.show()
+        img2.save(fname)
